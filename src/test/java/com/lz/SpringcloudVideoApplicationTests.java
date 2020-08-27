6437b7cd1de38590799d6aa0b9c6bfe195485d3a
lz <231135467>
2020-08-27 14:40:07 +0800

    springcloud 初始化

---
+++ src/test/java/com/lz/SpringcloudVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudVideoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
